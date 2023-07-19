805b02cdaf657671abd06249983c5ba300d96692
devLorran <lorranrodrigues553@gmail.com>
2023-07-19 12:57:20 -0300

    correções feitas

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -9,17 +9,17 @@ export const Projects = () => {
             <div className="project-card">
                 <span>Clone da NetFlix</span>
                 <p> Clone da Netflix feito com ReactJS HTML5 e CSS3 e media queries para deixar responsivo com o consumo de uma API chamada The Movie DB </p>
-                <a href="https://github.com/devLorran/clone-netflix">Ver Projeto</a>
+                <a href="https://github.com/devLorran/clone-netflix" target="_blank">Ver Projeto</a>
             </div>
             <div className="project-card">
                 <span>Movie Website</span>
                 <p> Site de filmes responsivo feito com HTML5 CSS3 e JavaScript e JSON para renderização dos filmes </p>
-                <a href="https://devlorran.github.io/movie-website/">Ver Projeto</a>
+                <a href="https://devlorran.github.io/movie-website/" target="_blank">Ver Projeto</a>
             </div>
             <div className="project-card">
                 <span>Dashboard</span>
                 <p> Dashboard responsivo feito com HTML5, CSS3, JavaScript e Vanilla para utilização de gráficos  </p>
-                <a href="https://devlorran.github.io/dashboard/">Ver Projeto</a>
+                <a href="https://devlorran.github.io/dashboard/" target="_blank">Ver Projeto</a>
             </div>
         </div>
         
